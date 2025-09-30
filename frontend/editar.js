c09b49057d914811ef699b9894b7cc31027d93c8
NYKSSilva <nicollysantossilva0508@gmail.com>
2025-09-30 11:03:01 -0300

    Editar

---
+++ frontend/editar.js
@@ -0,0 +1,62 @@
+const API = 'http://localhost:3000/alunos'
+
+const urlParametro = new URLSearchParams(window.location.search)
+const id = urlParametro.get("id")
+
+console.log("Id do aluno:", id)
+
+const inputID = document.getElementById("id")
+inputID.value = id
+
+async function salvar(e) {
+    e.preventDefault();
+    console.log("Salvando aluno");
+    const nome = inputNome.value.trim();
+    const cpf = inputCpf.value.trim();
+    const cep = inputCep.value.trim();
+    const uf = inputUf.value.trim();
+    const rua = inputRua.value.trim();
+    const numero = inputNumero.value.trim();
+    const complemento = inputComplemento.value.trim();
+    if(!nome && !cpf && !numero){
+        alert("Por gentileza preencha os campos obrigatorios")
+    }
+    const novoAluno = {
+        nome, cpf, cep, uf, rua, numero, complemento
+    }
+   
+   
+    try {
+        const requisicao = await fetch(API,{
+        method: "PUT",
+        headers: {"Content-Type" : "application/json"},
+        body: novoAluno? JSON.stringify(novoAluno): undefined
+    })
+        requisicao.status === 201? console.log(requisicao.json()): console.log("erro na requisição")
+    } catch (error) {
+        console.error(error)
+    }
+    
+    carregarTabela()
+}
+
+async function carregarAluno() {
+        if(!id){
+            alert("Nenhum aluno selecionado para a edição")
+            return
+        }
+        const resposta = await fetch(`${API}/${id}`);
+        const ALUNO = await resposta.json();
+        console.log(ALUNO)
+
+        document.getElementById("nome").value = ALUNO[0].nome;
+        document.getElementById("cpf").value = ALUNO[0].cpf;
+        document.getElementById("cep").value = ALUNO[0].cep;
+        document.getElementById("uf").value = ALUNO[0].uf;
+        document.getElementById("rua").value = ALUNO[0].nome;
+        document.getElementById("numero").value = ALUNO[0].numero;
+        document.getElementById("complemento").value = ALUNO[0].complemento;
+
+}
+
+carregarAluno()
